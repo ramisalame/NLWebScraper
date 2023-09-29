9f5dd2e93001a79b9f89303c8ff6ed0d93b54f0c
ramisalame <ramisalame@hotmail.com>
2023-09-29 17:54:52 -0400

    Create AutoBrow.py
    Opens news sites automatically

---
+++ AutoBrow.py
@@ -0,0 +1,14 @@
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+#Automatically starts chrome 
+driver = webdriver.Chrome()
+driver.get("https://www.ctvnews.ca/")
+
+
+#Wait a little for page to load 
+time.sleep(6)
