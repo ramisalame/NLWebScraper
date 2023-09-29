cd2ef208d11ea9af3d99ba66f2c7d7adbab8a211
ramisalame <ramisalame@hotmail.com>
2023-09-29 18:00:37 -0400

    multi Files

--- AutoBrow.py
+++ AutoBrow.py
@@ -5,6 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+import testFile
+
 #Automatically starts chrome 
 driver = webdriver.Chrome()
 driver.get("https://www.ctvnews.ca/")
@@ -12,3 +14,5 @@ driver.get("https://www.ctvnews.ca/")
 
 #Wait a little for page to load 
 time.sleep(6)
+
+print(a)
